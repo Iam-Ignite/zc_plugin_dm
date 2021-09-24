0a0f0f7aab87d9eee83a3c0041628f0e2b02f9bd
Faith Godwin <82447117+Iam-Ignite@users.noreply.github.com>
2021-09-24 15:19:35 +0100

    Delete dmreactplugin directory

--- dmreactplugin/src/Redux/App/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authedUserReducer from "../Reducers/AuthedUser";
-// import other reducers
-
-const rootReducer = combineReducers({
-//   Reducers go here
-    authedUser:authedUserReducer,
-});
-export default rootReducer;
