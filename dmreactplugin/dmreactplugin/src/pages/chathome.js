33571793a691d933bab48576a7762be5fd96974e
Faith Godwin <82447117+Iam-Ignite@users.noreply.github.com>
2021-09-24 15:18:21 +0100

    added the emoji picker component
    Click on the emoji sticker to view the popup options of emojis

---
+++ dmreactplugin/dmreactplugin/src/pages/chathome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmojiPicker from '../components/common/dmEmojiComp/dmEmojiComp';
+
+const ChatHome = () => {
+    return (
+        <div className='chat-home'>
+            This is the beginning of the chat home page.
+        </div>
+    );
+};
+
+export default ChatHome;
